3cdf6a2198f65c4524e00396cfe499bbdbfc16a5
claeys Emolo <claeysemolo@claeyss-MacBook-Pro.local>
2017-09-23 12:20:47 -0400

    first push

---
+++ config/auth.js
@@ -0,0 +1,17 @@
+module.exports = {
+  facebookAuth: {
+    clientID: '896605267054808',
+    clientSecret: '28f6a7d2842d5f9ba19e449131694902',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  },
+  twitterAuth: {
+    consumerKey: 'sOM9aZpoEyNAXKqvEUHd2O7cN',
+    consumerSecret: '6I1x0PgH41AxuqtAKcfRqYtWDMKNv0TBaaoegW3RP3o1ks7NPk',
+    callbackURL: 'http://localhost:3000/auth/twitter/callback',
+  },
+  googleAuth: {
+    clientID: '540016092664-27cdnbipjcao6gmtv0o3qaq52f1ehigi.apps.googleusercontent.com',
+    clientSecret: '-9PHhAhAhfqgSJy_c2pyp3bY',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
+  },
+};
