9d7794a2dd03681e6187536eb0dbe996e3dfcb88
claeys Emolo <claeysemolo@claeyss-MacBook-Pro.local>
2017-09-26 18:52:44 -0400

    second push

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 4000;
 
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: 'mongodb://localhost/expressauth',
+  url: 'mongodb://phil:pww@ds147034.mlab.com:47034/heroku_7pkzfm7p'
 };
